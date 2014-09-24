ba8a6b178217e74b16fcb099e1b58fbb48f10cde
Lance Alt <lancealt@gmail.com>
2014-09-23 22:16:42 -0700

    Removed duplicate copyright block.

--- src/plugins/Nothing/nothing.cpp
+++ src/plugins/Nothing/nothing.cpp
@@ -9,25 +9,6 @@
 **  experimental system. The Naval Postgraduate School assumes no responsibility
 **  whatsoever for its use by other parties, and makes no guarantees, expressed
 **  or implied, about its quality, reliability, or any other characteristic. We
-**  would appreciate acknowledgment if the software is used.
-**
-**  @file nothing.cpp
-**  @author Lance Alt (lancealt@gmail.com)
-**  @date 2014/09/01
-**
-*******************************************************************************/
-
-/******************************************************************************
-**
-**  This file is part of NpsGate.
-**
-**  This software was developed at the Naval Postgraduate School by employees
-**  of the Federal Government in the course of their official duties. Pursuant
-**  to title 17 Section 105 of the United States Code this software is not
-**  subject to copyright protection and is in the public domain. NpsGate is an
-**  experimental system. The Naval Postgraduate School assumes no responsibility
-**  whatsoever for its use by other parties, and makes no guarantees, expressed
-**  or implied, about its quality, reliability, or any other characteristic. We
 **  would appreciate acknowledgement if the software is used.
 **
 **  @file nothing.cpp
